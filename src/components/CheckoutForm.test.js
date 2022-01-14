ffb96c749eec77dfae496eb2817285f61fd0702f
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-01-14 17:29:54 -0600

    working on it

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,64 @@
 import React from "react";
-import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import MutationObserver from "mutationobserver-shim";
+
 import CheckoutForm from "./CheckoutForm";
+import PlantList from "./PlantList";
 
-// Write up the two tests here and make sure they are testing what the title shows
+describe("Checkout Form tests", () => {
+  test("renders without errors", () => {
+    render(<CheckoutForm />);
+  });
 
-test("renders without errors", () => {});
+  test("form submits correctly", async () => {
+    render(<CheckoutForm />);
 
-test("shows success message on submit with form details", () => {});
+    fireEvent.change(screen.getByLabelText(/first name:/i), {
+      target: { value: "test value first name" },
+    });
+    fireEvent.change(screen.getByLabelText(/last name/i), {
+      target: { value: "test value last name" },
+    });
+    fireEvent.change(screen.getByLabelText(/address/i), {
+      target: { value: "test value address" },
+    });
+    fireEvent.change(screen.getByLabelText(/city/i), {
+      target: { value: "test value city" },
+    });
+    fireEvent.change(screen.getByLabelText(/state/i), {
+      target: { value: "test value state" },
+    });
+    fireEvent.change(screen.getByLabelText(/zip/i), {
+      target: { value: "test value zip" },
+    });
+
+    fireEvent.click(screen.getByRole("button", { name: /checkout/i }));
+
+    expect(screen.getByTestId("successMessage")).toBeInTheDocument();
+  });
+
+  test("Fetches the list of plants", async () => {
+    render(<PlantList addToCart={jest.fn()} />);
+
+    await waitFor(() => screen.getAllByTestId("plant-card"));
+
+    expect(screen.getAllByTestId("plant-card")).toHaveLength(8);
+  });
+});
+
+describe("Plant List tests", () => {
+  test("renders without errors", () => {
+    render(<PlantList />);
+  });
+
+  test("Calls addToCart function when add button is clicked", async () => {
+    const addToCart = jest.fn();
+    render(<PlantList addToCart={addToCart} />);
+
+    await waitFor(() => screen.getAllByTestId("plant-card"));
+
+    fireEvent.click(screen.getAllByRole("button", { name: /Add to cart/i })[0]);
+
+    expect(addToCart).toHaveBeenCalled();
+  });
+});
